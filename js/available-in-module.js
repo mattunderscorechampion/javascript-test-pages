b2dc2b832422a93e529886b919c04cba300a5305
Matt Champion <mattunderscorechampion@gmail.com>
2017-07-01 15:48:23 +0100

    Import a module and report on its scope.

---
+++ js/available-in-module.js
@@ -0,0 +1,18 @@
+
+export function has_document() {
+    if (document) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+export function has_window() {
+    if (window) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
