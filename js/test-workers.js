a25575228f83d03f3c5045c66ede099fa163b08a
Matt Champion <mattunderscorechampion@gmail.com>
2018-05-15 22:35:40 +0100

    Check if 'ArrayBuffers' can be transfered to workers.

--- js/test-workers.js
+++ js/test-workers.js
@@ -5,4 +5,10 @@ self.addEventListener('message', function (msg) {
     if (msg.data === 'Hello') {
         self.postMessage(1);
     }
+    else if (msg.data instanceof self.ArrayBuffer) {
+        var view = new Int32Array(msg.data);
+        if (view[0] === 2) {
+            self.postMessage(3);
+        }
+    }
 });
